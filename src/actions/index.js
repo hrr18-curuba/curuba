e4fd3e9a187cdbb2eba8f398f3bda97f144d05ae
Tpredale <tpredale@gmail.com>
2016-09-27 14:20:24 -0400

    Check to see  if user still has a token, and then  keep them authenticated

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,7 +18,7 @@ export function signinUser({email, password}){
       localStorage.setItem('token', response.data.token);
 
 
-      browserHistory.push('/feature');
+      browserHistory.push('/recipes');
     })
     .catch(() => {
       dispatch(authError('Incorrect login information'));
@@ -33,7 +33,7 @@ export function signupUser({email, password}){
     .then(response => {
       dispatch({type: AUTH_USER});
       localStorage.setItem('token', response.data.token);
-      browserHistory.push('/feature');
+      browserHistory.push('/recipes');
     })
     .catch(response => dispatch(authError(response.data.error)));
   }
