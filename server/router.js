b6b613d9529613bdac9afce1452abbf4d7cd67df
Tpredale <tpredale@gmail.com>
2016-09-27 00:16:47 -0400

    Add Cors to allow our api server to receive requests from another url

--- server/router.js
+++ server/router.js
@@ -7,6 +7,9 @@ const requireSignin = passport.authenticate('local', {session: false})
 
 
 module.exports = function(app) {
+  app.get('/', requireAuth, function(req, res){
+    res.send({message: 'API Server'});
+  })
  app.post('/signin', requireSignin, Authentication.signin);
  app.post('/signup', Authentication.signup);
 }
--- server/server.js
+++ server/server.js
@@ -6,10 +6,12 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const cors = require('cors');
 
 
 //App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({type: '*/*'}));
 router(app);
 
@@ -17,15 +19,10 @@ const db =  process.env.MONGODB_URI || 'mongodb://localhost/auth:auth';
 mongoose.connect(db);
 
 //Server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3090;
 const server = http.createServer(app);
 
 
-
-
-
-
-//
 // app.use(express.static(__dirname + '/../src/public'));
 
 // app.get('*', function (req, res) {
