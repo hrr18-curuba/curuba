16dbd9403ec542ac092361bfdc12d7894400f2ce
Tpredale <tpredale@gmail.com>
2016-09-26 20:42:01 -0400

    added local strategy for passportjs

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,6 +3,22 @@ const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
+const LocalStrategy = require('passport-local');
+
+
+
+const localLogin = new LocalStrategy({ usernameField: 'email' }, function(email, password, done){
+  User.findOne({ email: email }, function(err, user){
+    if(err){ return done(err); }
+    if(!user) {return done(null, false);}
+    user.comparePassword(password, function(err, isMatch){
+      if(err) {return done(err); }
+      if(!isMatch) { return done(null, false); }
+
+      return done(null, user);
+    });
+  });
+})
 
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
@@ -22,3 +38,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done){
 });
 
 passport.use(jwtLogin);
+passport.use(localLogin);
