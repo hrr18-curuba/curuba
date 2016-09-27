a91ca2ee9a60cead4b75cad4d691339dddbd537c
Tpredale <tpredale@gmail.com>
2016-09-27 18:15:27 -0400

    Added whatwg-fetch module to enable a get request to yummly api

---
+++ src/api/recipeSearch.js
@@ -0,0 +1,14 @@
+import {appID, appKey} from '../../config';
+
+
+export default function recipeSearch(keyword, results, callback) {
+  fetch(`http://api.yummly.com/v1/api/recipes?_app_id=${appID}&_app_key=${appKey}&q=${keyword}
+&requirePictures=true`)
+  .then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    callback(data);
+  }).catch(function(err) {
+    console.log('Error ', err);
+  });
+}
