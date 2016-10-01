2f730d1d41eb1f599b16a1f17e6008deaf58b82c
Tpredale <tpredale@gmail.com>
2016-10-01 14:32:35 -0400

    Skeleton of the chef reducer creater

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,14 @@ import {combineReducers} from 'redux';
 import {reducer as form} from 'redux-form';
 import authReducer from './auth_reducer';
 import matches from './recipe_reducer';
+import chefsReducer from './chefs_reducer';
 
 
 const rootReducer = combineReducers({
   form: form,
   auth: authReducer,
-  matches: matches
+  matches: matches,
+  chefs: chefsReducer
 });
 
 export default rootReducer;
