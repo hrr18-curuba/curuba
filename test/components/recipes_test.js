2d7f1a5b317610b2eab769ea069e56d84401ad97
Tpredale <tpredale@gmail.com>
2016-10-02 15:20:03 -0400

    Testing for recipes component

--- test/components/recipes_test.js
+++ test/components/recipes_test.js
@@ -1,11 +1,18 @@
 import {renderComponent, expect} from '../test_helper';
-import Recipes from '../../src/components/Recipes';
+import Recipes from '../../src/components/recipes';
 
 describe ('Recipes', () =>{
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(Recipes);
+  });
+
+  it('has the correct class', () => {
+    expect(component).to.have.class('recipes');
+  });
+
   it('has a search input', () =>{
-
-    const component = renderComponent(Recipes);
-
     expect(component.find('input')).to.exist;
   });
 });
