6db39faf4f4ab56d2adb2afd8ffafd518c4043e5
Tpredale <tpredale@gmail.com>
2016-10-11 14:21:12 -0400

    Email to hire

--- src/containers/chef-detail.js
+++ src/containers/chef-detail.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router';
+
+
 
 class ChefDetail extends Component {
   render(){
@@ -11,6 +14,9 @@ class ChefDetail extends Component {
         <h3> Chef Details:</h3>
         <div> Chef Name: {this.props.chef.name}</div>
         <div> Specialties: {this.props.chef.specialties}</div>
+        <img className="img-responsive" src={this.props.chef.profile} alt="" />
+        <a className="btn btn-primary" href="mailto:hello@beunprovecho.com?subject=Hire%20Chef"> Hire Me </a>
+
        </div>
       );
   }
--- src/reducers/chefs_reducer.js
+++ src/reducers/chefs_reducer.js
@@ -4,6 +4,6 @@ export default function(){
   {name: 'Tom Doe', specialties: "Breakfasts, Pastries, Brunch, Finger Foods"},
   {name: 'Jane Doe', specialties: "Italian, Seafood, Tapas"},
   {name: 'Emma Smith', specialties: "Latin Fusion, Peruvian, Salads"},
-  {name: 'Frank Doe', specialties: "Cakes, Pastries, Cookies, Small Desserts"},
+  {name: 'Frank Doe', specialties: "Cakes, Pastries, Cookies, Small Desserts",  profile: 'http://mexico.gastronomia.com/uploads/chefs/b190733b2ea3eeec75bfa9a5ced6410c5e3f311f.jpeg' },
   ]
 }
