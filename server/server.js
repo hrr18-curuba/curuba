561099e999ff14c636abc1a8284579e7d1f566d8
Tpredale <tpredale@gmail.com>
2016-10-12 15:28:48 -0400

    Code Styling

--- server/server.js
+++ server/server.js
@@ -10,26 +10,28 @@ const router = require('./router');
 const cors = require('cors');
 
 
-//App Setup
+//  App Setup
+
 app.use(morgan('combined'));
 app.use(cors());
-app.use(bodyParser.json({type: '*/*'}));
+app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
 app.use(express.static(__dirname + "/../src/public"));
 
 app.get('*', (req, res) => {
- res.sendFile(path.join(__dirname, '/../src/public/index.html'));
+  res.sendFile(path.join(__dirname, '/../src/public/index.html'));
 });
 
-const db =  process.env.MONGODB_URI || 'mongodb://localhost/auth:auth';
+const db = process.env.MONGODB_URI || 'mongodb://localhost/auth:auth';
 mongoose.connect(db);
 
-//Server
+//  Server
+
 const PORT = process.env.PORT || 3090;
 const server = http.createServer(app);
 
 
-server.listen(PORT, () => {console.log("Listening on port 3090");});
+server.listen(PORT, () => { console.log('Listening on port 3090'); });
 
 module.exports = app;
